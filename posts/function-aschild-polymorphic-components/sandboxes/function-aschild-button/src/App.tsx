347577e1ecb7a4fa752557f908ff99291cf4084c
Nick Saunders <nick@saunde.rs>
2023-07-21 14:36:02 -0700

    Clean up poly button demo

--- posts/function-aschild-polymorphic-components/sandboxes/function-aschild-button/src/App.tsx
+++ posts/function-aschild-polymorphic-components/sandboxes/function-aschild-button/src/App.tsx
@@ -5,32 +5,31 @@ import Button from "./Button";
 
 function App() {
   return (
-    <div style={{ display: "flex", gap: 8, margin: 16 }}>
-      <form
-        style={{ display: "contents" }}
-        onSubmit={() => {
-          alert("form onSubmit callback");
-        }}>
-        <Button type="submit" variant="primary">
-          Submit
-        </Button>
-        <Button variant="secondary">
-          {({ className, disabled, ...restProps }) =>
-            exhausted(restProps) && (
-              <a
-                href="#/home"
-                className={className}
-                aria-disabled={disabled}
-                onClick={e => {
-                  if (disabled) e.preventDefault();
-                }}>
-                Cancel
-              </a>
-            )
-          }
-        </Button>
-      </form>
-    </div>
+    <form
+      style={{ display: "flex", gap: 8, margin: 16 }}
+      onSubmit={e => {
+        alert("form submit callback");
+        e.preventDefault();
+      }}>
+      <Button type="submit" variant="primary">
+        Submit
+      </Button>
+      <Button variant="secondary">
+        {({ className, disabled, ...restProps }) =>
+          exhausted(restProps) && (
+            <a
+              href="#/home"
+              className={className}
+              aria-disabled={disabled}
+              onClick={e => {
+                if (disabled) e.preventDefault();
+              }}>
+              Cancel
+            </a>
+          )
+        }
+      </Button>
+    </form>
   );
 }
 
